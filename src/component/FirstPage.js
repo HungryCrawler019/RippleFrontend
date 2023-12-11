87cd4354df1904861475b7ed8e1cbc172a370c67
tariqMah <t.ali1993@hotmail.com>
2023-12-11 11:28:27 +0400

    add helment to support meta keywords and title

--- src/component/FirstPage.js
+++ src/component/FirstPage.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import PeerTable from "./PeerTable";
 import ServerStateTable from "./ServerStateTable";
+import { Helmet } from "react-helmet";
 
 const FirstPage = () => {
   return (
     <div>
+      <Helmet>
+        <title>Xrp Kuwait Public Hub</title>
+        <meta name="description" content="Xrp Kuwait Public Hub" />
+      </Helmet>
       <ServerStateTable />
       <PeerTable />
     </div>
